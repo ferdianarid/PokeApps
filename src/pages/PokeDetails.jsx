91cc02d92206864f56998008e1310f325c72759a
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-09-10 10:04:41 +0700

    feature: develop pokemon details

--- src/pages/PokeDetails.jsx
+++ src/pages/PokeDetails.jsx
@@ -1,9 +1,15 @@
 import axios from 'axios'
+import styled from 'styled-components'
 import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
-import styled from 'styled-components'
+import SkillCard from '../components/organism/Cards/SkillCard'
 import { Heading, Subheading } from '../components/atoms/Text'
-import { Container } from '../components/atoms/Container'
+import { HeadingContent, PropsValue } from '../components/atoms/Text'
+import { PropsWrapper, AbilityWrapper, DetailWrapper } from '../components/atoms/Wrapper'
+import { AbilityBadge } from '../components/atoms/Badge'
+import SpritesPrev from '../components/organism/Cards/SpritesPrev'
+import FlavorCard from '../components/organism/Cards/FlavorCard'
+import { capitalizeFirstLetter } from '../helper/CapitalizeFirstLetter'
 
 const PokeDetails = () => {
     const location = useLocation()
@@ -22,57 +28,59 @@ const PokeDetails = () => {
             .then((response) => setDetails(response.data))
     }, [location.pathname])
 
-    const capitalizeFirstLetter = (string) => {
-        return string?.charAt(0).toUpperCase() + string?.slice(1);
-    }
+    const currentPath = location.pathname
 
-    const DetailWrapper = styled.div`
-        padding: 40px 0px;
-    `
-
-    const HeadingContent = styled.h3`
-        color: #FFF;
-    `
-
-    const TextContent = styled.p`
-        color: #FFF;
-        font-size: 14px;
-    `
-
-    const AbilityBadge = styled.p`
-        color: #FFF;
-        font-size: 14px;
-        padding: 8px 16px;
-        border-radius: 8px;
-        background: blue;
-        height: fit-content;
-    `
-
-    const AbilityWrapper = styled.div`
-        display: flex;
-        flex-direction: column;
-        grid-gap: 12px;
-    `
-
-    console.log(details)
     return (
         <Header>
-            <Heading>{capitalizeFirstLetter(details ? details.name : "")}</Heading>
-            <Subheading>Description about pokemon {details ? details.name : ""}</Subheading>
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+                <div>
+                    <Heading>{capitalizeFirstLetter(details ? details.name : "")}</Heading>
+                    <Subheading>Description about pokemon {details ? details.name : ""}</Subheading>
+                </div>
+                <h1 style={{ fontSize: "80px", color: "yellow" }}>#{currentPath.substring(currentPath.lastIndexOf("/") + 1, currentPath.length)}</h1>
+            </div>
             <DetailWrapper>
                 <div style={{ display: "flex", gridGap: "20px" }}>
                     <img src={`https://img.pokemondb.net/artwork/large/${details.name}.jpg`} width="250px" height="200px" style={{ borderRadius: "8px" }} />
                     <AbilityWrapper>
                         <HeadingContent>Abilities</HeadingContent>
                         <div style={{ display: "flex", alignItems: "center", gridGap: "8px" }}>
-                        {details.abilities?.map((item) => (
-                            <AbilityBadge>{item.ability.name}</AbilityBadge>
-                        ))}
+                            {details.abilities?.map((item) => (
+                                <AbilityBadge>{item.ability.name}</AbilityBadge>
+                            ))}
                         </div>
+                        <div style={{ width: "100%", display: "grid", gridTemplateColumns: "auto auto", gridGap: "12px" }}>
+                            <PropsWrapper>
+                                <HeadingContent>Information</HeadingContent>
+                                <PropsValue>Height : {details.height}</PropsValue>
+                                <PropsValue>Weight : {details.weight}</PropsValue>
+                                <PropsValue>Base Experience : {details.base_experience}</PropsValue>
+                                <PropsValue>Species : {capitalizeFirstLetter(details.species?.name)}</PropsValue>
+                            </PropsWrapper>
+
+                            <PropsWrapper>
+                                <HeadingContent>Sprites Preview</HeadingContent>
+                                <SpritesPrev sprites={details.sprites} />
+                            </PropsWrapper>
+                        </div>
+
+                        <PropsWrapper>
+                            <HeadingContent>Skills</HeadingContent>
+                            {details.abilities?.map((item) => (
+                                <SkillCard skills={item.ability?.url} />
+                            ))}
+                        </PropsWrapper>
+
+                        <PropsWrapper>
+                            <HeadingContent>Flavour</HeadingContent>
+                            {details.abilities?.map((item) => (
+                                <FlavorCard flavors={item.ability?.url} />
+                            ))}
+                        </PropsWrapper>
                     </AbilityWrapper>
                 </div>
             </DetailWrapper>
-        </Header>
+        </Header >
     )
 }
 
